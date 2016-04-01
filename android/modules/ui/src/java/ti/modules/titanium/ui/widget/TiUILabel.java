7184c06f56386f81c7da580c93f858c6e9ee41af
kjy8252 <kjy8252@kjy8252ui-Mac-mini.local>
2016-04-01 15:14:08 +0900

    about Label Longtouch event

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUILabel.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUILabel.java
@@ -81,6 +81,16 @@ public class TiUILabel extends TiUIView
 			}
 			
 			@Override
+			public boolean performLongClick(){
+				try{
+					return super.performLongClick();
+				}
+				catch(Exception e){
+					return true;
+				}
+			}
+			
+			@Override
 			public boolean onTouchEvent(MotionEvent event) {
 			        TextView textView = (TextView) this;
 			        Object text = textView.getText();
