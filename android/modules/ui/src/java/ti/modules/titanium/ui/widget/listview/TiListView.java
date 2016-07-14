78ed633d378abddbae4e79c9d8b925eb217ebf5c
hscho <hscho@linearhub.com>
2016-07-14 21:00:06 +0900

    TiListView crash 예외처리

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
@@ -1040,7 +1040,10 @@ public class TiListView extends TiUIView implements OnSearchChangeListener {
 					@Override
 					public void run() 
 					{
-						listView.setSelection(position + 1);
+						//hscho crash예외처리
+						if(listView!=null){
+							listView.setSelection(position + 1);
+						}
 					}
 				});
 			}
