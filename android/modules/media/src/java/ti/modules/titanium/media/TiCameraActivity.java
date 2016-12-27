073d9ec39ca183ad9d3268fb340c6b452a71e4a7
hscho <hscho@linearhub.com>
2016-12-27 22:08:32 +0900

    android camera preview 화각 조절

--- android/modules/media/src/java/ti/modules/titanium/media/TiCameraActivity.java
+++ android/modules/media/src/java/ti/modules/titanium/media/TiCameraActivity.java
@@ -124,7 +124,7 @@ public class TiCameraActivity extends TiBaseActivity implements SurfaceHolder.Ca
 		{
 			final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
 			final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
-			setMeasuredDimension(width, height);
+			//setMeasuredDimension(width, height);
 		
 			int previewWidth = MeasureSpec.getSize(widthMeasureSpec);
 			int previewHeight = MeasureSpec.getSize(heightMeasureSpec);
@@ -145,11 +145,13 @@ public class TiCameraActivity extends TiBaseActivity implements SurfaceHolder.Ca
 				}
 			}
 			if (previewHeight < previewWidth / aspectRatio) {
-				previewHeight = (int) (previewWidth / aspectRatio + .5);
-
+				//previewHeight = (int) (previewWidth / aspectRatio + .5);
+				previewWidth = (int) (previewWidth / aspectRatio + .5);
 			} else {
-				previewWidth = (int) (previewHeight * aspectRatio + .5);
+				//previewWidth = (int) (previewHeight * aspectRatio + .5);
+				previewHeight = (int) (previewHeight * aspectRatio + .5);
 			}
+			setMeasuredDimension(previewWidth, previewHeight);
 			
 			super.onMeasure(MeasureSpec.makeMeasureSpec(previewWidth,
 					MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(
